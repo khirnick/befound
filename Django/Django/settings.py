e05d8fba473fe08811613c45dc5ac330cc456b5d
Kiryanenko <kiryanenkoav@mail.ru>
2017-04-05 22:31:55 +0300

    Add modular-admin template

--- Django/Django/settings.py
+++ Django/Django/settings.py
@@ -119,4 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join('static'), )
